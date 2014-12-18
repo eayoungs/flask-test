509a7e15dc3e52819061368e847fad6bf14ae642
Eric Youngson <eric@successionecological.com>
2014-12-17 21:05:28 -0800

    -Added a function to connect to the database (users?)

--- flaskr.py
+++ flaskr.py
@@ -5,3 +5,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
 # Application
 app = flask(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
