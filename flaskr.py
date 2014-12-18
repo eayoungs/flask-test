0a91934855e3e0c41f2b852c89459081e14ad693
Eric Youngson <eric@successionecological.com>
2014-12-17 21:13:07 -0800

    -Fixed a typo to make the local instance of 'F'lask consistent

--- flaskr.py
+++ flaskr.py
@@ -3,7 +3,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
 
 # Application
-app = flask(__name__)
+app = Flask(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
