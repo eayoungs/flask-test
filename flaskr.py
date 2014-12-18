81d67e5132fa7a4811cba622f755df7f37de4c5c
Eric Youngson <eric@successionecological.com>
2014-12-17 20:58:11 -0800

    -Split configuration from app module to stand alone file... -Changed the function call in app module to call stand alone file with environment variable

--- flaskr.py
+++ flaskr.py
@@ -2,13 +2,6 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
 
-# Config
-DATABASE = '/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
 # Application
 app = flask(__name__)
-app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
