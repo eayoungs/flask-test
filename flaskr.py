590fde3e20b9d63b72e8847a6cf8ee93fccb80e9
Eric Youngson <eric@successionecological.com>
2015-01-21 09:09:37 -0800

    *flaskr.py -Added a new decorated function to handle showing all entries in the SQLite db...

--- flaskr.py
+++ flaskr.py
@@ -32,5 +32,12 @@ def teardown_request(exception):
         db.close()
 
 
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
